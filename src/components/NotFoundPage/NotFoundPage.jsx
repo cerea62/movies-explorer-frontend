9662355b3a0f122748abd9acfc93df94c7820146
cerea62 <cerea62@yandex.ru>
2024-02-07 15:41:21 +0300

    maked NotFoundPage

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './NotFoundPage.css'
+import { Link } from 'react-router-dom'
 
 export default function NotFoundPage() {
     return (
-        <h1 className='register'>NotFoundPage</h1>
+        <>
+                <div className='error__container'>
+                    <h1 className='error__title'>404</h1>
+                    <p className='error__caption'>Страница не найдена</p>
+                    {/* <Link className='error__link' to={''}>Назад</Link> */}
+                </div>
+               <div className='error__link-container'>
+                <Link className='error__link' to={''}>Назад</Link>
+                </div> 
+        </>
     )
 }
 
