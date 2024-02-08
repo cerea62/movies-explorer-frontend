ee7010555601c8342380571ae0844b9e9f5324d9
cerea62 <cerea62@yandex.ru>
2024-02-08 10:13:36 +0300

    maked components Auth and Login

---
+++ src/components/Auth/Auth.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Auth.css';
+import authLogo from '../../image/logo.jpg';
+import { Link } from 'react-router-dom';
+
+export default function Auth({ title, buttonText, onSubmit, loginStatus, linkText, children }) {
+    return (
+        <>
+            <div className="auth__container">
+                <img className='auth__logo' src={authLogo} alt="Логотип Место" />
+                <h2 className="auth__title">{title}</h2>
+                <form className="auth__form" onSubmit={onSubmit}>
+                    {children}
+                    <button type="submit" className="auth__submit">{buttonText}</button>
+                </form>
+                <p className='auth__text'>{loginStatus}{' '}
+                    <Link className='auth__link' to='/sign-in'>{linkText}
+                    </Link>
+                </p>
+            </div>
+        </>
+    )
+}
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import './Login.css'
+import Auth from '../Auth/Auth'
 
 export default function Login() {
     return (
-        <h1 className='login'>Login</h1>
+        <>
+            <Auth
+                title={'Рады видеть!'}
+                buttonText={'Войти'}
+                loginStatus={'Еще не зарегистрированы?'}
+                linkText={'Регистрация'}
+            // onSubmit={onChange}
+            >
+                <label className='input__label' for="Email">E-mail</label>
+                <input
+                    className="input"
+                    name="Email" type="email"
+                    // placeholder="Email"
+                    //   value={email}
+                    //   onChange={handleEmailChange}
+                    required
+                />
+                <label className='input__label' for="Password">Пароль</label>
+                <input className="input"
+                    name="Password"
+                    type="password"
+                    // placeholder="Пароль"
+                    //   value={password}
+                    //   onChange={handlePasswordChange}
+                    autoComplete="on"
+                    required
+                />
+            </Auth>
+        </>
     )
 }
 
