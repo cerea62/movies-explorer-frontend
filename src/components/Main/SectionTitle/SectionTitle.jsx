dd0044fca94980e40b870271e49aa08f99651bfa
cerea62 <cerea62@yandex.ru>
2024-02-19 22:10:34 +0300

    check to pixelPerfect

--- src/components/Main/SectionTitle/SectionTitle.jsx
+++ src/components/Main/SectionTitle/SectionTitle.jsx
@@ -3,6 +3,6 @@ import './SectionTitle.css'
 
 export default function SectionTitle({ title }) {
     return (
-            <h2 className="title__text text">{title}</h2>
+            <h2 className="title text">{title}</h2>
     )
 }
